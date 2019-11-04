f67fb425c0ac7be3508d7cbbb89341d161ce099c
Luis <>
2019-11-04 12:33:37 +0100

    Clases de pruebas creadas

---
+++ test/view/InicioFXControllerIT.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.testfx.framework.junit.ApplicationTest;
+
+/**
+ *
+ * @author 2dam
+ */
+public class InicioFXControllerIT extends ApplicationTest{
+    
+    public InicioFXControllerIT() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
