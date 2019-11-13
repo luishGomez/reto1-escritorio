71a0ba7a48cf57304050f8bdbfee0bae07a25c39
Usuario <Usuario@DESKTOP-2QMCK54>
2019-11-13 18:51:15 +0100

    Evaluacion 02.

--- src/view/PrincipalFXController.java
+++ src/view/PrincipalFXController.java
@@ -1,12 +1,15 @@
 package view;
 
 import clases.User;
+import java.net.URL;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
@@ -31,10 +34,15 @@ public class PrincipalFXController {
     private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger("view.PrincipalFXController");
     private User user;
     private Stage stage;
+    private Stage stageAyuda;
     @FXML
     private MenuBar menuBar;
     @FXML
     private Menu menuOpciones;
+    @FXML
+    private Menu menuAyuda;
+    @FXML
+    private MenuItem menuAyudaContenido;
     @FXML
     private MenuItem menuOpcionesSalir;
     @FXML
@@ -74,6 +82,12 @@ public class PrincipalFXController {
             menuOpcionesSalir.setText("_Salir");
             menuOpcionesCerrarSesion.setMnemonicParsing(true);
             menuOpcionesCerrarSesion.setText("_Cerrar sesión");
+            /*Modificación DIN 13/1/2019*/
+            menuAyuda.setMnemonicParsing(true);
+            menuAyuda.setText("_Ayuda");
+            menuAyudaContenido.setMnemonicParsing(true);
+            menuAyudaContenido.setText("Co_ntenido de ayuda");
+            /*Fin de la modificación de DIN 13/11/2019*/
             stage.show();
         }catch(Exception e){
             LOGGER.severe(e.getMessage());
@@ -142,15 +156,52 @@ public class PrincipalFXController {
             LOGGER.severe(e.getMessage());
         }
     }
+    /**
+     * Inserta el usuario de la sesión iniciada.
+     * Inserts the user of the login.
+     * @param user Los datos del usuario. / User data.
+     */
     public void setUser(User user){
         this.user=user;
     }
     /*Modificación DIN 13/1/2019*/
+    @FXML
     public void ayuda(){
+        LOGGER.info("algo");
         final WebView browser = new WebView();
         final WebEngine webEngine = browser.getEngine();
-        webEngine.load("./ayudaPrincipal.html");
         
-        VBox vBox = new VBox(browser);
+        URL url = this.getClass().getResource("./ayudaPrincipal.html");
+        webEngine.load(url.toString());
+        
+        stageAyuda=new Stage();
+        stageAyuda.setTitle(webEngine.getTitle());
+        
+        Button ayudaCerrar=new Button("Cerrar");        
+        ayudaCerrar.setOnAction(this::cerrarAyuda);
+        ayudaCerrar.setMnemonicParsing(true);
+        ayudaCerrar.setText("_Cerrar");
+        /* Al pulsar enter encima del boton se ejecute */
+        ayudaCerrar.setOnKeyPressed((key) ->{
+            if(key.getCode().equals(KeyCode.ENTER)) {
+                 ayudaCerrar.fire();
+            }
+        });
+        stageAyuda.setOnShowing((event)->{
+            ayudaCerrar.requestFocus();
+            
+        });
+        VBox root = new VBox();
+        root.getChildren().addAll(browser,ayudaCerrar);
+        
+        Scene escenaAyuda=new Scene(root);
+        stageAyuda.setScene(escenaAyuda);
+        stageAyuda.initModality(Modality.APPLICATION_MODAL);
+        
+        stageAyuda.show();
     }
+    public void cerrarAyuda(ActionEvent event){
+        stageAyuda.hide();
+    }
+    /*Fin de la modificación de DIN 13/11/2019*/
 }
---
+++ src/view/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Es el paquete que engloba todas las clases de vistas como archivos FXML's de
+ * toda la aplicación del lado cliente en escritorio.
+ */
+package view;
